81f1842a5b7a6d134703521d2a9048001bb230b9
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-07-12 10:34:46 +0200

    Split into subfiles

---
+++ lib/FilterEngine.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def remove_phase_discontinuities(phase: list) -> list:
+    """
+    In a wrapped phase array, replaces the two points before and after
+    each wrap with NaN. This is useful to prevent matplotlib from plotting
+    vertical lines at the wrap locations
+
+    Args:
+        phase (list): phase points list
+
+    Returns:
+        list: input list with NaN before and after each wrap
+    """
+
+    # Get sign of each point
+    # Negative  gives -1
+    # Positive  gives 1
+    # Zero      gives 0
+    signs = np.sign(phase)
+
+    # Split the list in adjacent pairs
+    pairs = []
+    for i in range(len(signs) - 1):
+        pairs.append( (signs[i], signs[i + 1]) )
+
+    # A pair of (-1, 1) indicates a wrap: find out the indexes of such pairs
+    pairs_indexes = [index for index, element in enumerate(pairs) if element == (-1, 1) or element == (1, -1)]
+
+    # Replace all points before the wraps with NaN
+    phase_nan = phase.copy()
+    phase_nan[pairs_indexes] = np.nan
+
+    return phase_nan
