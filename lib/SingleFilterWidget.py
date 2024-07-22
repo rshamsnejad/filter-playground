7e06b0ddb1c320e3c4d6e5876cf7b47bd6d9e30a
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-07-22 15:24:41 +0200

    Boxify parameter fields

--- lib/SingleFilterWidget.py
+++ lib/SingleFilterWidget.py
@@ -24,49 +24,52 @@ class SingleFilterWidget(QWidget):
         self.__filterengine = FilterEngine(self.__layout)
 
         self.init_type_box()
-        self.init_order_field()
-        self.init_cutoff_field()
+        self.init_parameter_box()
 
         self.show()
 
     def init_type_box(self):
-        button_box = QVBoxLayout()
+        button_layout = QVBoxLayout()
         types = ["Highpass", "Lowpass"]#, "Bandpass", "Bandstop"]
         radio_buttons = []
         for type in types:
             radio_buttons.append(QRadioButton(type, self))
             radio_buttons[-1].toggled.connect(self.handle_type)
 
-            button_box.addWidget(radio_buttons[-1])
+            button_layout.addWidget(radio_buttons[-1])
 
         radio_buttons[0].setChecked(True)
 
-        button_group = QGroupBox("Filter type")
-        button_group.setLayout(button_box)
+        button_box = QGroupBox("Filter type")
+        button_box.setLayout(button_layout)
 
-        self.__layout.addWidget(button_group, 0, 0, 1, 1)
+        self.__layout.addWidget(button_box, 0, 0, 1, 1)
 
-    def init_order_field(self):
+    def init_parameter_box(self):
+        parameter_layout = QGridLayout()
+
         label_order = QLabel("Filter order:")
-
         field_order = QLineEdit(str(self.__filterengine.get_order()), self)
         field_order.placeholderText = "Order"
         field_order.maxLength = 3
         field_order.textChanged.connect(self.handle_order)
 
-        self.__layout.addWidget(label_order, 0, 1, 1, 1)
-        self.__layout.addWidget(field_order, 0, 2, 1, 1)
+        parameter_layout.addWidget(label_order, 0, 0, 1, 1)
+        parameter_layout.addWidget(field_order, 0, 1, 1, 1)
 
-    def init_cutoff_field(self):
         label_cutoff = QLabel("Cutoff frequency:")
-
         field_cutoff = QLineEdit(str(self.__filterengine.get_cutoff()), self)
         field_cutoff.placeholderText = "Cutoff"
         field_cutoff.maxLength = 5
         field_cutoff.textChanged.connect(self.handle_cutoff)
 
-        self.__layout.addWidget(label_cutoff, 1, 1, 1, 1)
-        self.__layout.addWidget(field_cutoff, 1, 2, 1, 1)
+        parameter_layout.addWidget(label_cutoff, 1, 0, 1, 1)
+        parameter_layout.addWidget(field_cutoff, 1, 1, 1, 1)
+
+        parameter_box = QGroupBox("Filter parameters")
+        parameter_box.setLayout(parameter_layout)
+
+        self.__layout.addWidget(parameter_box, 0, 1, 1, 1)
 
 
     def handle_type(self):
