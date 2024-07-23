71b896920900584fc33206e61ee6d4df2408c9a4
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-07-23 14:17:22 +0200

    Remove old classes

--- lib/InputFilterWidget.py
+++ lib/InputFilterWidget.py
@@ -1,97 +1,59 @@
 import logging
 from PyQt6.QtWidgets import (
     QWidget,
-    QGridLayout,
-    QVBoxLayout,
-    QGroupBox,
-    QRadioButton,
-    QLineEdit,
-    QLabel
+    QVBoxLayout
 )
-from lib.FilterEngine import FilterEngine
-
+from lib.FilterToolbarWidget import FilterToolbarWidget
+from lib.GraphWidget import GraphWidget
+from lib.ButterEngine import ButterEngine
 
 class InputFilterWidget(QWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.__layout = QGridLayout()
-        self.setLayout(self.__layout)
+        self.setLayout(QVBoxLayout())
 
-        self.__filterengine = FilterEngine(self.__layout)
+        self.graph = GraphWidget(ButterEngine())
+        self.filter_toolbar = FilterToolbarWidget()
 
-        self.init_type_box()
-        self.init_parameter_box()
+        for button in self.filter_toolbar.filter_type.radio_buttons:
+            button.toggled.connect(self.handle_type)
 
-        self.show()
+        self.filter_toolbar.filter_parameters.field_order.textChanged.connect(self.handle_order)
+        self.filter_toolbar.filter_parameters.field_cutoff.textChanged.connect(self.handle_cutoff)
 
-    def init_type_box(self):
-        button_layout = QVBoxLayout()
-        types = ["Highpass", "Lowpass"]#, "Bandpass", "Bandstop"]
-        radio_buttons = []
-        for type in types:
-            radio_buttons.append(QRadioButton(type, self))
-            radio_buttons[-1].toggled.connect(self.handle_type)
-
-            button_layout.addWidget(radio_buttons[-1])
-
-        radio_buttons[0].setChecked(True)
-
-        button_box = QGroupBox("Filter type")
-        button_box.setLayout(button_layout)
-
-        self.__layout.addWidget(button_box, 0, 0, 1, 1)
-
-    def init_parameter_box(self):
-        parameter_layout = QGridLayout()
-
-        label_order = QLabel("Filter order:")
-        field_order = QLineEdit(str(self.__filterengine.get_order()), self)
-        field_order.placeholderText = "Order"
-        field_order.maxLength = 3
-        field_order.textChanged.connect(self.handle_order)
-
-        parameter_layout.addWidget(label_order, 0, 0, 1, 1)
-        parameter_layout.addWidget(field_order, 0, 1, 1, 1)
-
-        label_cutoff = QLabel("Cutoff frequency:")
-        field_cutoff = QLineEdit(str(self.__filterengine.get_cutoff()), self)
-        field_cutoff.placeholderText = "Cutoff"
-        field_cutoff.maxLength = 5
-        field_cutoff.textChanged.connect(self.handle_cutoff)
-
-        parameter_layout.addWidget(label_cutoff, 1, 0, 1, 1)
-        parameter_layout.addWidget(field_cutoff, 1, 1, 1, 1)
-
-        parameter_box = QGroupBox("Filter parameters")
-        parameter_box.setLayout(parameter_layout)
-
-        self.__layout.addWidget(parameter_box, 0, 1, 1, 1)
-
+        self.layout().addWidget(self.filter_toolbar)
+        self.layout().addWidget(self.graph)
 
     def handle_type(self):
         rb = self.sender()
 
-        self.__filterengine.set_filtertype(rb.text())
-        self.__filterengine.update_filter()
+        try:
+            self.graph.engine.set_filtertype(rb.text() or 'highpass')
+        except ValueError as e:
+            logging.warning(e)
+
+        self.graph.engine.compute()
+        self.graph.update_graph()
 
     def handle_order(self):
         rb = self.sender()
 
         try:
-            self.__filterengine.set_order(int(rb.text() or 1))
+            self.graph.engine.set_order(int(rb.text() or 1))
         except ValueError as e:
             logging.warning(e)
 
-        self.__filterengine.update_filter()
+        self.graph.engine.compute()
+        self.graph.update_graph()
 
     def handle_cutoff(self):
         rb = self.sender()
 
         try:
-            self.__filterengine.set_cutoff(int(rb.text() or 1000))
+            self.graph.engine.set_cutoff(int(rb.text() or 1000))
         except ValueError as e:
             logging.warning(e)
 
-        self.__filterengine.update_filter()
-
+        self.graph.engine.compute()
+        self.graph.update_graph()
