ea5789a0a4e377b99c26af596392799dbdb20667
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-07-25 17:23:57 +0200

    Fix spinbox widths

--- lib/FilterParametersWidget.py
+++ lib/FilterParametersWidget.py
@@ -19,11 +19,14 @@ class FilterParametersWidget(QGroupBox):
         self.setTitle("Filter parameters")
         self.setLayout(QGridLayout())
 
+        spinbox_width = 120
+
         label_order = QLabel("Filter order:")
         self.field_order = QSpinBox()
         self.field_order.setMinimum(1)
         self.field_order.setMaximum(10)
         self.field_order.setValue(order)
+        self.field_order.setFixedWidth(spinbox_width)
 
         self.layout().addWidget(label_order, 0, 0, 1, 1)
         self.layout().addWidget(self.field_order, 0, 1, 1, 1)
@@ -33,6 +36,7 @@ class FilterParametersWidget(QGroupBox):
         self.field_cutoff.setMinimum(1)
         self.field_cutoff.setMaximum(100000)
         self.field_cutoff.setValue(cutoff)
+        self.field_cutoff.setFixedWidth(spinbox_width)
 
         self.layout().addWidget(label_cutoff, 1, 0, 1, 1)
         self.layout().addWidget(self.field_cutoff, 1, 1, 1, 1)
@@ -42,6 +46,7 @@ class FilterParametersWidget(QGroupBox):
         self.field_gain.setMinimum(-100)
         self.field_gain.setMaximum(100)
         self.field_gain.setValue(gain)
+        self.field_gain.setFixedWidth(spinbox_width)
 
         self.layout().addWidget(label_gain, 0, 2, 1, 1)
         self.layout().addWidget(self.field_gain, 0, 3, 1, 1)
@@ -51,6 +56,7 @@ class FilterParametersWidget(QGroupBox):
         self.field_Q.setMinimum(0)
         self.field_Q.setMaximum(100)
         self.field_Q.setValue(Q)
+        self.field_Q.setFixedWidth(spinbox_width)
 
         self.layout().addWidget(label_Q, 1, 2, 1, 1)
         self.layout().addWidget(self.field_Q, 1, 3, 1, 1)
