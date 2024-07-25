6471f737be76fb3f70def60a74e13079e4320a29
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-07-25 14:34:38 +0200

    Add full order support for allpass

--- lib/BiquadEngine.py
+++ lib/BiquadEngine.py
@@ -1,6 +1,6 @@
 from lib.GraphEngine import GraphEngine
 from scipy.signal import butter, freqz
-from numpy import logspace, log10, angle, pi, sin, cos, sqrt
+from numpy import logspace, log10, angle, pi, sin, cos, sqrt, tan, convolve
 
 class BiquadEngine(GraphEngine):
     def __init__(self, *args, **kwargs) -> None:
@@ -12,13 +12,40 @@ class BiquadEngine(GraphEngine):
         self.alpha = sin(self.w0) / (2 * self.Q)
 
         if self.get_filtertype().lower() in ["highpass", "lowpass"]:
-            b, a = butter(self.order, self.cutoff, self.filtertype, fs=self.fs)
+            self.b, self.a = butter(self.order, self.cutoff, self.filtertype, fs=self.fs)
 
         elif self.get_filtertype().lower() == "allpass":
-            b = [1 - self.alpha, -2 * cos(self.w0), 1 + self.alpha]
-            a = [1 + self.alpha, -2 * cos(self.w0), 1 - self.alpha]
+            coeff1 = (tan(pi * self.get_cutoff()/self.fs) - 1) / (tan(pi * self.get_cutoff()/self.fs) + 1)
+            b_order1 = [coeff1, 1]
+            a_order1 = [1, coeff1]
+            b_order2 = [1 - self.alpha, -2 * cos(self.w0), 1 + self.alpha]
+            a_order2 = [1 + self.alpha, -2 * cos(self.w0), 1 - self.alpha]
 
-        frequencies, magnitude = freqz(b, a, worN=logspace(0, 5, 1000), fs=self.fs)
+            if self.get_order() == 1:
+                self.b = b_order1
+                self.a = a_order1
+            elif self.get_order() == 2:
+                self.b = b_order2
+                self.a = a_order2
+            else:
+                # Odd order of 3 or more
+                if self.get_order() % 2 == 1:
+                    iterations = (self.get_order() - 1) / 2
+
+                    self.b = b_order1
+                    self.a = a_order1
+                # Even order of 4 or more
+                else:
+                    iterations = self.get_order() / 2
+
+                    self.b = b_order2
+                    self.a = b_order2
+
+                for i in range(int(iterations)):
+                    self.b = convolve(self.b, b_order2)
+                    self.a = convolve(self.a, a_order2)
+
+        frequencies, magnitude = freqz(self.b, self.a, worN=logspace(0, 5, 1000), fs=self.fs)
 
         mag_db = 20 * log10(abs(magnitude))
         phase_deg = angle(magnitude, deg=True)
@@ -38,7 +65,7 @@ class BiquadEngine(GraphEngine):
                 f", order {self.get_order()}"
             )
         elif self.get_filtertype().lower() == "allpass":
-            type_string = f"Biquad allpass filter, order 2, Q = {self.Q:.2f}"
+            type_string = f"Biquad allpass filter, order {self.get_order()}, Q = {self.Q:.2f}"
 
         return type_string + (
             f", $f_0 = {self.get_cutoff()}$ Hz"
