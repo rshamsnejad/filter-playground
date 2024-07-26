5f59eda62c4a3dd33373c0ea3ee4fa272e508b92
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-07-26 09:48:09 +0200

    switch compute method to use match-case

--- lib/BiquadEngine.py
+++ lib/BiquadEngine.py
@@ -10,39 +10,44 @@ class BiquadEngine(GraphEngine):
         self.w0 = 2 * pi * self.get_frequency() / self.fs
         self.alpha = sin(self.w0) / (2 * self.Q)
 
-        if self.get_filtertype().lower() in ["highpass", "lowpass"]:
-            self.b, self.a = butter(self.get_order(), self.get_frequency(), self.get_filtertype(), fs=self.fs)
+        match self.get_filtertype().lower():
+            case "highpass" | "lowpass":
+                self.b, self.a = butter(self.get_order(), self.get_frequency(), self.get_filtertype(), fs=self.fs)
 
-        elif self.get_filtertype().lower() == "allpass":
-            coeff1 = (tan(pi * self.get_frequency()/self.fs) - 1) / (tan(pi * self.get_frequency()/self.fs) + 1)
-            b_order1 = [coeff1, 1]
-            a_order1 = [1, coeff1]
-            b_order2 = [1 - self.alpha, -2 * cos(self.w0), 1 + self.alpha]
-            a_order2 = [1 + self.alpha, -2 * cos(self.w0), 1 - self.alpha]
+            case "allpass":
+                coeff1 = (tan(pi * self.get_frequency()/self.fs) - 1) / (tan(pi * self.get_frequency()/self.fs) + 1)
+                b_order1 = [coeff1, 1]
+                a_order1 = [1, coeff1]
+                b_order2 = [1 - self.alpha, -2 * cos(self.w0), 1 + self.alpha]
+                a_order2 = [1 + self.alpha, -2 * cos(self.w0), 1 - self.alpha]
 
-            if self.get_order() == 1:
-                self.b = b_order1
-                self.a = a_order1
-            elif self.get_order() == 2:
-                self.b = b_order2
-                self.a = a_order2
-            else:
-                # Odd order of 3 or more
-                if self.get_order() % 2 == 1:
-                    iterations = (self.get_order() - 1) / 2
-
+                if self.get_order() == 1:
                     self.b = b_order1
                     self.a = a_order1
-                # Even order of 4 or more
-                else:
-                    iterations = self.get_order() / 2
-
+                elif self.get_order() == 2:
                     self.b = b_order2
-                    self.a = b_order2
+                    self.a = a_order2
+                else:
+                    # Odd order of 3 or more
+                    if self.get_order() % 2 == 1:
+                        iterations = (self.get_order() - 1) / 2
 
-                for i in range(int(iterations)):
-                    self.b = convolve(self.b, b_order2)
-                    self.a = convolve(self.a, a_order2)
+                        self.b = b_order1
+                        self.a = a_order1
+                    # Even order of 4 or more
+                    else:
+                        iterations = self.get_order() / 2
+
+                        self.b = b_order2
+                        self.a = b_order2
+
+                    for i in range(int(iterations)):
+                        self.b = convolve(self.b, b_order2)
+                        self.a = convolve(self.a, a_order2)
+
+
+            case _:
+                raise ValueError("Unknown filter type")
 
         frequencies, magnitude = freqz(self.b, self.a, worN=self.frequency_points, fs=self.fs)
 
@@ -58,15 +63,19 @@ class BiquadEngine(GraphEngine):
         self.remove_phase_discontinuities()
 
     def generate_title(self) -> str:
-        if self.get_filtertype().lower() in ["highpass", "lowpass"]:
-            type_string = (
-                f"Butterworth {self.get_filtertype().lower()} filter"
-                # f", order {self.get_order()}"
-            )
-        elif self.get_filtertype().lower() == "allpass":
-            type_string = (
-                f"Biquad allpass filter"
-                # ", order {self.get_order()}, Q = {self.Q:.2f}"
-            )
+        match self.get_filtertype().lower():
+            case "highpass" | "lowpass":
+                type_string = (
+                    f"Butterworth {self.get_filtertype().lower()} filter"
+                    # f", order {self.get_order()}"
+                )
+            case "allpass":
+                type_string = (
+                    f"Biquad allpass filter"
+                    # ", order {self.get_order()}, Q = {self.Q:.2f}"
+                )
+
+            case _:
+                raise ValueError("Unknown filter type")
 
         return type_string # + f", $f_0 = {self.get_frequency()}$ Hz"
