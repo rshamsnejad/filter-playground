5d068e0d82b10a5faad8bbc1bb1f89be732c8d3a
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-07-26 11:05:41 +0200

    Use compute_and_update

--- lib/InputGraphWidget.py
+++ lib/InputGraphWidget.py
@@ -4,8 +4,7 @@ class InputGraphWidget(GraphWidget):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.engine.compute()
-        self.update_graph()
+        self.compute_and_update()
 
     def update_axvlines(self) -> None:
         self.axline_mag[0].set_xdata([self.engine.get_frequency()])
