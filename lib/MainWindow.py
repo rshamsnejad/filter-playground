a6b2e11129a95ef9e1727a31702302f4c03f336b
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-08-12 16:00:50 +0200

    Remove unused code

--- lib/MainWindow.py
+++ lib/MainWindow.py
@@ -1,12 +1,5 @@
-from PyQt6.QtWidgets import (
-    QMainWindow,
-    QToolBar,
-    QSpinBox,
-    QLabel
-)
-from PyQt6.QtGui import (
-    QIcon
-)
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtGui import QIcon
 from lib.MainWidget import MainWidget
 
 class MainWindow(QMainWindow):
@@ -22,21 +15,5 @@ class MainWindow(QMainWindow):
 
         self.main_widget = MainWidget()
         self.setCentralWidget(self.main_widget)
-
-        # self.spinbox = QSpinBox()
-        # self.spinbox.setMinimum(1)
-        # self.spinbox.setMaximum(100)
-        # self.spinbox.setValue(2)
-        # self.spinbox.setFixedWidth(40)
-        # for cascade_widget in self.main_widget.input_widget.cascade_widgets:
-        #     self.spinbox.valueChanged.connect(cascade_widget.update_input_filter_amount)
-
-        # self.toolbar = QToolBar('Main toolbar')
-        # self.toolbar.setFloatable(False)
-        # self.toolbar.setMovable(False)
-        # self.toolbar.addWidget(QLabel("Amount of input filters:"))
-        # self.toolbar.addWidget(self.spinbox)
-
-        # self.addToolBar(self.toolbar)
 
         self.show()
