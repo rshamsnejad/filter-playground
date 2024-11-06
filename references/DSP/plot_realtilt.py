17134351636e54e8e0e19c69f84653251ca6933f
Robin SHAMSNEJAD <rshamsnejad@arkamys.com>
2024-11-06 09:39:58 +0100

    Update tilt

--- references/DSP/plot_realtilt.py
+++ references/DSP/plot_realtilt.py
@@ -4,27 +4,29 @@ from scipy import signal
 import numpy as np
 
 ### Parameters
-alpha = -0.1
-N = 9
-r = 2
+alpha = -0.2
+N = 20
+fs = 48000
+fmin = 20
+fmax = fs / 2
 
 ### Poles, zeros and gain computation
+r = (fmax / fmin) ** (1 / (N-1))
 pole_spacing = np.log(r)
 zero_spacing = -alpha * pole_spacing
 
-a_poles = [-50]
+a_poles = [-fmin]
 
 for i in range(1, N):
     a_poles.append( a_poles[0] * r ** i )
 
-a_zeros = np.array(a_poles) * r ** (-alpha)
+a_zeros = np.array(a_poles) * (r ** (-alpha))
 
 b, a = signal.zpk2tf(a_zeros, a_poles, 1)
 
 # Gain scaling to get unity at DC
 a_gain = a[-1] / b[-1]
 
-fs = 48000
 a_prewarped_poles = np.array(
     a_poles[0] * np.tan( np.pi * np.array(a_poles) / fs ) / np.tan( np.pi * a_poles[0] / fs )
 )
@@ -43,6 +45,7 @@ a_frequencies, a_magnitude = signal.freqs_zpk(
 a_mag_db = 20 * np.log10(abs(a_magnitude))
 a_phase_deg = np.angle(a_magnitude, deg=True)
 
+sos = signal.zpk2sos(d_zeros, d_poles, d_gain)
 d_frequencies, d_magnitude = signal.freqz_zpk(
     d_zeros, d_poles, d_gain,
     worN=frequency_points,
@@ -56,7 +59,7 @@ plot_freq_range     = [0.1, 10e5]
 plot_phase_range    = [-200, 200]
 
 fig, axs = plt.subplots(2, 2, sharex=False)
-fig.suptitle(f"Tilt filter, N={N}, $ \\alpha $={alpha}")
+fig.suptitle(f"Tilt filter, N={N}, $\\alpha$={alpha}, $F_s$={fs}")
 
 # Analog magnitude
 axs[0][0].set_title("Analog")
@@ -86,7 +89,7 @@ axs[1][0].axvline(20e3, color='green', linestyle='--')
 axs[1][0].axvline(fs / 2, color='red', linestyle='--')
 
 # Digital magnitude
-axs[0][1].set_title("Digital")
+axs[0][1].set_title(f"Digital ({len(sos)} biquads)")
 axs[0][1].semilogx(d_frequencies, d_mag_db)
 axs[0][1].set_xlim(plot_freq_range)
 axs[0][1].grid(which='both', axis='both')
@@ -110,4 +113,5 @@ axs[1][1].axvline(20, color='green', linestyle='--')
 axs[1][1].axvline(20e3, color='green', linestyle='--')
 axs[1][1].axvline(fs / 2, color='red', linestyle='--')
 
+fig.tight_layout()
 plt.show()
